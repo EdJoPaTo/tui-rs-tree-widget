b941b070d6166d099b8544daa7c0d6e5ae37446b
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-04-30 22:38:16 +0200

    test(bench): add benchmark

---
+++ benches/bench.rs
@@ -0,0 +1,121 @@
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use ratatui::buffer::Buffer;
+use ratatui::layout::Rect;
+use ratatui::widgets::StatefulWidget;
+use tui_tree_widget::{Tree, TreeItem, TreeState};
+
+fn example_items() -> Vec<TreeItem<'static, &'static str>> {
+    vec![
+        TreeItem::new_leaf("a", "Alfa"),
+        TreeItem::new(
+            "b",
+            "Bravo",
+            vec![
+                TreeItem::new_leaf("c", "Charlie"),
+                TreeItem::new(
+                    "d",
+                    "Delta",
+                    vec![
+                        TreeItem::new_leaf("e", "Echo"),
+                        TreeItem::new_leaf("f", "Foxtrot"),
+                    ],
+                )
+                .expect("all item identifiers are unique"),
+                TreeItem::new_leaf("g", "Golf"),
+            ],
+        )
+        .expect("all item identifiers are unique"),
+        TreeItem::new_leaf("h", "Hotel"),
+        TreeItem::new(
+            "i",
+            "India",
+            vec![
+                TreeItem::new_leaf("j", "Juliett"),
+                TreeItem::new_leaf("k", "Kilo"),
+                TreeItem::new_leaf("l", "Lima"),
+                TreeItem::new_leaf("m", "Mike"),
+                TreeItem::new_leaf("n", "November"),
+            ],
+        )
+        .expect("all item identifiers are unique"),
+        TreeItem::new_leaf("o", "Oscar"),
+        TreeItem::new(
+            "p",
+            "Papa",
+            vec![
+                TreeItem::new_leaf("q", "Quebec"),
+                TreeItem::new_leaf("r", "Romeo"),
+                TreeItem::new_leaf("s", "Sierra"),
+                TreeItem::new_leaf("t", "Tango"),
+                TreeItem::new_leaf("u", "Uniform"),
+                TreeItem::new(
+                    "v",
+                    "Victor",
+                    vec![
+                        TreeItem::new_leaf("w", "Whiskey"),
+                        TreeItem::new_leaf("x", "Xray"),
+                        TreeItem::new_leaf("y", "Yankee"),
+                    ],
+                )
+                .expect("all item identifiers are unique"),
+            ],
+        )
+        .expect("all item identifiers are unique"),
+        TreeItem::new_leaf("z", "Zulu"),
+    ]
+}
+
+fn init(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group("init");
+
+    group.bench_function("empty", |bencher| {
+        bencher.iter(|| {
+            black_box(Tree::<usize>::new(black_box(vec![])).unwrap());
+        });
+    });
+
+    group.bench_function("example-items", |bencher| {
+        bencher.iter(|| {
+            black_box(Tree::new(black_box(example_items())).unwrap());
+        });
+    });
+
+    group.finish();
+}
+
+fn renders(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group("render");
+
+    let buffer_size = Rect::new(0, 0, 100, 50);
+
+    let tree = Tree::new(vec![]).unwrap();
+    group.bench_function("empty", |bencher| {
+        bencher.iter_batched(
+            || (tree.clone(), TreeState::<usize>::default()),
+            |(tree, mut state)| {
+                let mut buffer = Buffer::empty(buffer_size);
+                black_box(tree).render(buffer_size, black_box(&mut buffer), &mut state);
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    let tree = Tree::new(example_items()).unwrap();
+    group.bench_function("example-items", |bencher| {
+        bencher.iter_batched(
+            || (tree.clone(), TreeState::default()),
+            |(tree, mut state)| {
+                let mut buffer = Buffer::empty(buffer_size);
+                black_box(tree).render(buffer_size, black_box(&mut buffer), &mut state);
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, init, renders);
+criterion_main!(benches);
