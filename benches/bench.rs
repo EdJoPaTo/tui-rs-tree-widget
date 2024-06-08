9e86018f24c6bb6904091b370eebbf209d243e30
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-06-08 16:58:51 +0200

    test(bench): dont require feature
    compile them dynamically in

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,7 +4,6 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughpu
 use ratatui::buffer::Buffer;
 use ratatui::layout::Rect;
 use ratatui::widgets::StatefulWidget;
-use tui_tree_widget::third_party::json::Selector as JsonSelector;
 use tui_tree_widget::{Tree, TreeItem, TreeState};
 
 #[must_use]
@@ -69,61 +68,6 @@ fn example_items() -> Vec<TreeItem<'static, &'static str>> {
     ]
 }
 
-fn metadata() -> serde_json::Value {
-    let output = std::process::Command::new("cargo")
-        .arg("metadata")
-        .arg("--format-version=1")
-        .arg("--all-features")
-        .output()
-        .unwrap();
-    assert!(
-        output.status.success(),
-        "cargo metadata should be executed successfully"
-    );
-    let stdout = String::from_utf8(output.stdout).expect("Should be able to parse metadata");
-    let metadata: serde_json::Value = serde_json::from_str(&stdout).unwrap();
-    metadata
-}
-
-fn open_all(
-    state: &mut TreeState<Vec<JsonSelector>>,
-    json: &serde_json::Value,
-    selector: &[JsonSelector],
-) {
-    match json {
-        serde_json::Value::Null
-        | serde_json::Value::Bool(_)
-        | serde_json::Value::Number(_)
-        | serde_json::Value::String(_) => {}
-        serde_json::Value::Array(array) if array.is_empty() => {}
-        serde_json::Value::Array(array) => {
-            state.open(selector.to_vec());
-            for (index, value) in array.iter().enumerate() {
-                let mut child_selector = selector.to_vec();
-                child_selector.push(JsonSelector::Index(index));
-                open_all(state, value, &child_selector);
-            }
-        }
-        serde_json::Value::Object(object) if object.is_empty() => {}
-        serde_json::Value::Object(object) => {
-            state.open(selector.to_vec());
-            for (key, value) in object {
-                let mut child_selector = selector.to_vec();
-                child_selector.push(JsonSelector::Key(key.to_string()));
-                open_all(state, value, &child_selector);
-            }
-        }
-    }
-}
-
-fn key(key: &str) -> JsonSelector {
-    JsonSelector::Key(key.to_owned())
-}
-
-const fn index(index: usize) -> JsonSelector {
-    JsonSelector::Index(index)
-}
-
 fn init(criterion: &mut Criterion) {
     let mut group = criterion.benchmark_group("init");
     group.throughput(Throughput::Elements(1)); // Frames per second
@@ -142,11 +86,10 @@ fn init(criterion: &mut Criterion) {
         });
     });
 
-    // There is no point in creating TreeData from JSON as that only has impact on render.
-    // This is interesting for comparison to the main branch / other approaches
-    // TODO: remove before merge to main
-    let metadata = metadata();
+    // While the metadata is quite big its only a reference with no executions -> fast
+    #[cfg(feature = "json")]
     group.bench_function("metadata", |bencher| {
+        let metadata = json::metadata();
         bencher.iter(|| {
             let _: Tree<_> = black_box(Tree::new(black_box(&metadata)));
         });
@@ -195,60 +138,126 @@ fn renders(criterion: &mut Criterion) {
         );
     });
 
-    let metadata = metadata();
+    #[cfg(feature = "json")]
+    {
+        let metadata = json::metadata();
 
-    group.bench_function("metadata/no_open", |bencher| {
-        let mut state = TreeState::default();
-        bencher.iter_batched(
-            || Buffer::empty(buffer_size),
-            |mut buffer| {
-                Tree::new(black_box(&metadata)).render(
-                    buffer_size,
-                    black_box(&mut buffer),
-                    black_box(&mut state),
-                );
-            },
-            BatchSize::SmallInput,
-        );
-    });
+        group.bench_function("metadata/no_open", |bencher| {
+            let mut state = TreeState::default();
+            bencher.iter_batched(
+                || Buffer::empty(buffer_size),
+                |mut buffer| {
+                    Tree::new(black_box(&metadata)).render(
+                        buffer_size,
+                        black_box(&mut buffer),
+                        black_box(&mut state),
+                    );
+                },
+                BatchSize::SmallInput,
+            );
+        });
 
-    group.bench_function("metadata/few_open", |bencher| {
-        let mut state = TreeState::default();
-        state.open(vec![key("packages")]);
-        state.open(vec![key("packages"), index(0)]);
-        state.open(vec![key("resolve")]);
-        state.open(vec![key("resolve"), key("nodes")]);
-        state.open(vec![key("resolve"), key("nodes"), index(0)]);
-        bencher.iter_batched(
-            || Buffer::empty(buffer_size),
-            |mut buffer| {
-                Tree::new(black_box(&metadata)).render(
-                    buffer_size,
-                    black_box(&mut buffer),
-                    black_box(&mut state),
-                );
-            },
-            BatchSize::SmallInput,
-        );
-    });
+        group.bench_function("metadata/few_open", |bencher| {
+            use json::{index, key};
+            let mut state = TreeState::default();
+            state.open(vec![key("packages")]);
+            state.open(vec![key("packages"), index(0)]);
+            state.open(vec![key("resolve")]);
+            state.open(vec![key("resolve"), key("nodes")]);
+            state.open(vec![key("resolve"), key("nodes"), index(0)]);
+            bencher.iter_batched(
+                || Buffer::empty(buffer_size),
+                |mut buffer| {
+                    Tree::new(black_box(&metadata)).render(
+                        buffer_size,
+                        black_box(&mut buffer),
+                        black_box(&mut state),
+                    );
+                },
+                BatchSize::SmallInput,
+            );
+        });
 
-    group.bench_function("metadata/all_open", |bencher| {
-        let mut state = TreeState::default();
-        open_all(&mut state, &metadata, &[]);
-        bencher.iter_batched(
-            || Buffer::empty(buffer_size),
-            |mut buffer| {
-                Tree::new(black_box(&metadata)).render(
-                    buffer_size,
-                    black_box(&mut buffer),
-                    black_box(&mut state),
-                );
-            },
-            BatchSize::SmallInput,
-        );
-    });
+        group.bench_function("metadata/all_open", |bencher| {
+            let mut state = TreeState::default();
+            json::open_all(&mut state, &metadata, &[]);
+            bencher.iter_batched(
+                || Buffer::empty(buffer_size),
+                |mut buffer| {
+                    Tree::new(black_box(&metadata)).render(
+                        buffer_size,
+                        black_box(&mut buffer),
+                        black_box(&mut state),
+                    );
+                },
+                BatchSize::SmallInput,
+            );
+        });
+    }
 
     group.finish();
+}
+
+/// Helpers for json releated benches
+#[cfg(feature = "json")]
+mod json {
+    use tui_tree_widget::third_party::json::Selector;
+    use tui_tree_widget::TreeState;
+
+    pub fn key(key: &str) -> Selector {
+        Selector::Key(key.to_owned())
+    }
+
+    pub const fn index(index: usize) -> Selector {
+        Selector::Index(index)
+    }
+
+    pub fn metadata() -> serde_json::Value {
+        let output = std::process::Command::new("cargo")
+            .arg("metadata")
+            .arg("--format-version=1")
+            .arg("--all-features")
+            .output()
+            .unwrap();
+        assert!(
+            output.status.success(),
+            "cargo metadata should be executed successfully"
+        );
+        let stdout = String::from_utf8(output.stdout).expect("Should be able to parse metadata");
+        let metadata: serde_json::Value = serde_json::from_str(&stdout).unwrap();
+        metadata
+    }
+
+    pub fn open_all(
+        state: &mut TreeState<Vec<Selector>>,
+        json: &serde_json::Value,
+        selector: &[Selector],
+    ) {
+        match json {
+            serde_json::Value::Null
+            | serde_json::Value::Bool(_)
+            | serde_json::Value::Number(_)
+            | serde_json::Value::String(_) => {}
+            serde_json::Value::Array(array) if array.is_empty() => {}
+            serde_json::Value::Array(array) => {
+                state.open(selector.to_vec());
+                for (index, value) in array.iter().enumerate() {
+                    let mut child_selector = selector.to_vec();
+                    child_selector.push(self::index(index));
+                    open_all(state, value, &child_selector);
+                }
+            }
+            serde_json::Value::Object(object) if object.is_empty() => {}
+            serde_json::Value::Object(object) => {
+                state.open(selector.to_vec());
+                for (key, value) in object {
+                    let mut child_selector = selector.to_vec();
+                    child_selector.push(self::key(key));
+                    open_all(state, value, &child_selector);
+                }
+            }
+        }
+    }
 }
 
 /// Create flamegraphs with `cargo bench --bench bench -- --profile-time=5`
